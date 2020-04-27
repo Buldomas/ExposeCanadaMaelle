03b794d90b20693e21f462d9c1fb98ab8e207364
Buldomas <laurent.soubigou445@orange.fr>
2020-04-27 14:28:30 +0200

    Ajout Villes

---
+++ Climats.js
@@ -0,0 +1,35 @@
+/* Arctique */
+var Cl1 = document.getElementById("Cl1");
+Cl1.addEventListener("click", function()  {
+    location.href = "./arctique.html";
+});
+/* subarctique */
+var Cl2 = document.getElementById("Cl2");
+Cl2.addEventListener("click", function()  {
+    location.href = "./subarctique.html";
+});
+/* continental sec */
+var Cl3 = document.getElementById("Cl3");
+Cl3.addEventListener("click", function()  {
+    location.href = "./continentalsec.html";
+});
+/* continental humide */
+var Cl4 = document.getElementById("Cl4");
+Cl4.addEventListener("click", function()  {
+    location.href = "./continentalhumide.html";
+});
+/* montagne */
+var Cl5 = document.getElementById("Cl5");
+Cl5.addEventListener("click", function()  {
+    location.href = "./montagne.html";
+});
+/* maritime Est */
+var Cl6 = document.getElementById("Cl6");
+Cl6.addEventListener("click", function()  {
+    location.href = "./maritime_est.html";
+});
+/* maritime Ouest */
+var Cl7 = document.getElementById("Cl7");
+Cl7.addEventListener("click", function()  {
+    location.href = "./maritime_ouest.html";
+});
