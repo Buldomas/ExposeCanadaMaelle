b50624dcef75dbe334a8170ca1ffd556165a2112
Buldomas <laurent.soubigou445@orange.fr>
2020-04-28 10:50:45 +0200

    Carrousel mieux

--- carrousel.js
+++ carrousel.js
@@ -5,6 +5,22 @@ var
 	numImages = figure.childElementCount,
 	theta =  2 * Math.PI / numImages,
 	currImage = 0
+	currPoss = 0
+	nameVille = [
+		'Victoria - Colombie Britanique',
+		'Charlottetown - Île du Prince Edward',
+		'Winnipeg - Manitoba',
+		'Fredericton - New Brunswick',
+		'Iqualuit - Ninavut',
+		'Halifax - Nouvelle Ecosse',
+		'Ottawa - Ontario',
+		'Toronto - Ontario',
+		'Québec - Québec',
+		'Regina - Saskatchewan',
+		'Saint John\'s - Terre Neuve',
+		'Yellowknife - Territoire du Nord Ouest',
+		'Whitehorse - Yukon',
+		'Edmonton - Alberta']
 ;	
 nav.addEventListener('click', onClick, true);
 function onClick(e) {
@@ -16,10 +32,21 @@ function onClick(e) {
 	
 	if (t.classList.contains('next')) {
 		currImage++;
+		currPoss++;
+		if (currPoss == 14) {
+			currPoss = 0;
+		}
 	}
 	else {
 		currImage--;
+		currPoss--;
+		if (currPoss == -1) {
+			currPoss = 13;
+		}
 	}
-	
+	var position = document.getElementById("nomville");
+	var pville = nameVille[currPoss]
+	position.textContent = pville;
+
 	figure.style.transform = `rotateY(${currImage * -theta}rad)`;
 }
